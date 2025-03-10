dce2deb6875fe3efaf7284ce466d084d09d64df8
aaq <152490665+isramasjid@users.noreply.github.com>
2025-03-10 19:59:56 -0400

    nonworking calculator

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let initialValue = 7
 
+function incrementValue() {
+  initialValue = initialValue + 1
+}
 
-document.getElementById("display").innerHTML = '0';
+document.getElementById("display").innerHTML = initialValue;
+
+const incrementButton = document.getElementById('increment')
+incrementButton.addEventListener('click', incrementValue())
