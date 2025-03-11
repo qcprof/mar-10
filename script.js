e332b52a5d301d4e52f7f72d81da6a6dde788a84
aaq <152490665+isramasjid@users.noreply.github.com>
2025-03-10 20:39:16 -0400

    working increment and decrement

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ function incrementValue() {
   document.getElementById("display").innerHTML = initialValue;
 }
 
-
+function decrementValue() {
+  initialValue = initialValue - 1
+  document.getElementById("display").innerHTML = initialValue;
+}
 
 
 const incrementButton = document.getElementById('increment')
 incrementButton.addEventListener('click', incrementValue)
+
+const decrementButton = document.getElementById('decrement')
+decrementButton.addEventListener('click', decrementValue)
